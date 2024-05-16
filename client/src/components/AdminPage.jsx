fe8fa6d50b67f54a14e3564aee6f246621c44242
Sumit <sumit.tpt07@gmail.com>
2024-05-17 00:09:21 +0530

    update

--- client/src/components/AdminPage.jsx
+++ client/src/components/AdminPage.jsx
@@ -31,9 +31,9 @@ const AdminPage = () => {
             <div className="relative overflow-x-auto p-5 rounded-md border-4 border-black">
               <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="px-6 py-3 rounded-s-lg">
-                      Kid name
+                  <tr className="">
+                    <th scope="col" className="px-6 py-3 ">
+                      Kid name 
                     </th>
                     <th scope="col" className="px-6 py-3">
                       Contact
@@ -41,16 +41,16 @@ const AdminPage = () => {
                     <th scope="col" className="px-6 py-3">
                       Age
                     </th>
-                    <th scope="col" className="px-6 py-3 rounded-e-lg">
+                    <th scope="col" className="px-6 py-3">
                       Adult Socks
                     </th>
-                    <th scope="col" className="px-6 py-3 rounded-e-lg">
+                    <th scope="col" className="px-6 py-3">
                       Kids Socks
                     </th>
-                    <th scope="col" className="px-6 py-3 rounded-e-lg">
+                    <th scope="col" className="px-6 py-3">
                       Payment Method
                     </th>
-                    <th scope="col" className="px-6 py-3 rounded-e-lg">
+                    <th scope="col" className="px-6 py-3">
                       Total Cost
                     </th>
                   </tr>
